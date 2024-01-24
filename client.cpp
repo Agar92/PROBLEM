05ee5e0ef3415bfa3209d8703439f8bf88246023
And <50169003+Agar92@users.noreply.github.com>
2024-01-24 11:25:57 +0300

    Add files via upload

---
+++ client.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <string>
+#include <boost/asio.hpp>
+#include <boost/algorithm/string/case_conv.hpp>
+
+using namespace boost::asio;
+
+//$g++ client.cpp -o client -lboost_system -lpthread
+
+int main()  {
+  try {
+    io_context io_context;
+    boost::asio::ip::tcp::socket socket{io_context};
+    socket.connect(ip::tcp::endpoint(ip::tcp::v4(),1895));
+    //while(true)
+    {
+      std::string input;
+      std::cin>>input;
+      std::string msg=input;
+      msg+='\n';
+      boost::system::error_code error;
+      boost::asio::async_write(socket,
+                               boost::asio::buffer(msg),
+                               [](boost::system::error_code ec,
+                                  size_t transferred){
+                                 std::cout<<"async_write Completion!"<<std::endl;
+                                 if(!ec)
+                                   std::cout<<"Client sent message!"<<std::endl;
+                                 else
+                                   std::cout<<"Client did not send the message!"<<std::endl;
+                               });
+      std::string response;
+      std::cout<<"BEFORE"<<std::endl;
+      std::cout<<"socket.local_endpoint():   "<<socket.local_endpoint()<<std::endl;
+      std::cout<<"socket.remote_endpoint():   "<<socket.remote_endpoint()<<std::endl;
+      boost::system::error_code err;
+      boost::asio::async_read_until(socket,
+                                    dynamic_buffer(response), '\n',
+                                    [] (boost::system::error_code ec,
+                                        std::size_t length) {
+                                      if(ec) std::cout<<"Error!!!"<<std::endl;
+                                      
+                                    });
+      std::cout<<"AFTER"<<std::endl;
+      if(error && error != boost::asio::error::eof)
+      {
+        std::cout<<"Read failed!"<<error.message()<<std::endl;
+      }
+      else
+      {
+        std::cout<<"Reading..."<<std::endl;
+        std::cout<<response
+                 <<response.length()
+                 <<std::endl;
+        for(int i=0; i<15; ++i) std::cout<<unsigned(response[i])<<" ";
+        std::cout<<std::endl;
+        response.clear();
+      }
+    }
+  }
+  catch (std::exception& e)
+  {
+    std::cerr << e.what() << std::endl;
+  }
+}
---
+++ server.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+#include <string>
+#include <boost/asio.hpp>
+#include <boost/algorithm/string/case_conv.hpp>
+#include <memory>
+
+//g++ server.cpp -o server -lboost_system -lpthread
+
+using namespace boost::asio;
+
+struct Session : std::enable_shared_from_this<Session> {
+  explicit Session(ip::tcp::socket socket) : socket{ std::move(socket) } { }
+  void read() {
+    async_read_until(socket, dynamic_buffer(message), '\n',
+            [self=shared_from_this()] (boost::system::error_code ec,
+                                       std::size_t length) {
+              std::cout<<"#1#:   "<<self->message<<std::endl;
+              if (ec || self->message == "\n") return;
+              std::cout<<"#2#:   "<<self->message<<std::endl;
+              boost::algorithm::to_upper(self->message);
+              std::cout<<"#3#:   "<<self->message<<std::endl;
+              self->write();
+              std::cout<<"#4#:   "<<self->message<<std::endl;
+            });
+  }
+  void write() {
+    std::cout<<"@1@:   "<<message<<std::endl;
+    message+="\n";
+    async_write(socket, buffer(message),
+                         [self=shared_from_this()] (boost::system::error_code ec,
+                                           std::size_t length) {
+                  std::cout<<"***A***:   "<<self->message
+                           <<" length="<<length
+                           <<std::endl;
+                  if (ec) return;
+                  self->message.clear();
+                  std::cout<<"***B***:   "<<self->message
+                           <<" length="<<length
+                           <<std::endl;
+                  self->read();
+                });
+    std::cout<<"@2@:   "<<message<<std::endl;
+  }
+private:
+  ip::tcp::socket socket;
+  std::string message;
+};
+
+void serve(ip::tcp::acceptor& acceptor) {
+  acceptor.async_accept([&acceptor](boost::system::error_code ec,
+                                    ip::tcp::socket socket) {
+    std::cout<<"socket.local_endpoint():   "<<socket.local_endpoint()<<std::endl;
+    std::cout<<"socket.remote_endpoint():   "<<socket.remote_endpoint()<<std::endl;
+    serve(acceptor);
+    if (ec) return;
+    auto session = std::make_shared<Session>(std::move(socket));
+    session->read();
+  });
+}
+
+int main()  {
+  try {
+    io_context io_context;
+    ip::tcp::acceptor acceptor{ io_context,
+                                ip::tcp::endpoint(ip::tcp::v4(), 1895) };
+    serve(acceptor);
+    io_context.run();
+  } catch (std::exception& e) {
+    std::cerr << e.what() << std::endl;
+  }
+}
